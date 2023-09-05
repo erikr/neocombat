34bf17ae447e02d8492364a19b8d464f5e367485
Erik Reinertsen <erikrtn@gmail.com>
2023-09-05 13:39:25 -0700

    Format decimals in counter print

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def main():
             break
 
         fractionComplete = frameCounter / totalFrames
-        print(f"Frame {frameCounter:0f} / {totalFrames:0f} ({fractionComplete * 100:.0f}%)")
+        print(f"Frame {frameCounter:.0f} / {totalFrames:.0f} ({fractionComplete * 100:.1f}%)")
         frameCounter = frameCounter + 1
 
     results = model.predict(
